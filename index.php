ad704b66369bc58a92fb84df0e8c73a0bf194f9c
Vaibhav Tiwari <89778487+VaibhavTiwari00@users.noreply.github.com>
2022-11-18 22:50:19 +0530

    check button

--- index.php
+++ index.php
@@ -108,7 +108,7 @@ if (isset($_POST['name']) && $_POST['name'] != ""  && isset($_POST['mail']) && $
         <div class="header-content ">
 
             <p>Most trusted Digital Partner for your </p>
-            <h1 data-aos="fade-in">We not only Create your Website but also Manage it FREE for 1 year</h1>
+            <h1 >We not only Create your Website but also Manage it FREE for 1 year</h1>
             <p class="main-landing-txt">
                 <span> <b> Get your website designed and managed at just $5 per hour</b> </span><br>
                 With our top-notch experts, you can be sure that your site will be managed perfectly. Our team of experts will help you make your site professional, responsive and optimized for conversions. We provide the best service at the cheapest price.
